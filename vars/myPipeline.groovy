adc91687d60ec3a30177f0d72183df09b5fb7948
Magno Almeida <77936130+Magnowz@users.noreply.github.com>
2025-02-17 17:46:34 -0300

    teste

--- vars/myPipeline.groovy
+++ vars/myPipeline.groovy
@@ -2,21 +2,18 @@ def call(Map config = [:]) {
     pipeline {
         agent any
         parameters {
-            // Define um parâmetro para carregar o arquivo JMX stashed
             stashedFile(config.scriptName ?: 'script.jmx')
         }
         stages {
             stage('Ler jmx') {
                 steps {
                     script {
-                        // Descompacta o arquivo stashed
                         unstash config.scriptName ?: 'script.jmx'
                         sh 'cat script.jmx'    
-                        // Executa algum comando (exemplo: sleep)
                         if (config.command) {
                             sh config.command
                         } else {
-                            echo "Nenhum comando específico foi configurado."
+                            echo ""
                         }
                     }
                 }

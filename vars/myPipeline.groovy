c46d87c9ca76029229170f1f1058b1a8c2caeced
Magno Almeida <77936130+Magnowz@users.noreply.github.com>
2025-02-18 08:15:28 -0300

    Update myPipeline.groovy

--- vars/myPipeline.groovy
+++ vars/myPipeline.groovy
@@ -8,7 +8,7 @@ def call(Map config = [:]) {
             stage('Ler jmx') {
                 steps {
                     script {
-                        currentBuild.displayName  = "${env.BUILD_NUMBER}-teste-api"
+                        currentBuild.displayName  = "${env.BUILD_NUMBER} teste-api 1.0.1 - 10000"
                         unstash config.scriptName ?: 'script.jmx'
                         sh 'cat script.jmx'    
                         if (config.command) {

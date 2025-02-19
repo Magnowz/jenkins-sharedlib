ca60e5810a690b00645755487d21091264b99dbd
Magno Almeida <77936130+Magnowz@users.noreply.github.com>
2025-02-19 18:27:02 -0300

    Teste jmeter

--- vars/jenkinsLib.groovy
+++ vars/jenkinsLib.groovy
@@ -11,7 +11,8 @@ def call(Map config = [:]) {
                        currentBuild.displayName  = "#${env.BUILD_NUMBER} teste-api 1.0.1 - 10000"
                         unstash config.scriptName ?: 'script.jmx'
                         //sh 'sleep 60'
-                        sh 'cat script.jmx'    
+                        //sh 'cat script.jmx'   
+                        sh 'jmeter -n -t script.jmx'
                         if (config.command) {
                             sh config.command
                         } else {

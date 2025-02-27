38a048b167d5b8e8b4c4009bf60b95cedaf4cf7b
Magno Almeida <77936130+Magnowz@users.noreply.github.com>
2025-02-27 18:28:32 -0300

    ajuste de build

--- vars/jenkinsLib.groovy
+++ vars/jenkinsLib.groovy
@@ -13,8 +13,8 @@ def call(Map config = [:]) {
                         //sh 'sleep 60'
                         //sh 'cat script.jmx'   
                         //sh "jmeter -n -t script.jmx -l report${env.BUILD_NUMBER}.jtl -g -o report${env.BUILD_NUMBER}"
-                        sh "jmeter -n -t script.jmx -l report${env.BUILD_NUMBER}.jtl -e -o report${env.BUILD_NUMBER}}"
-                        //sh "zip -r report${env.BUILD_NUMBER}.zip report${env.BUILD_NUMBER}"
+                        sh "jmeter -n -t script.jmx -l report${env.BUILD_NUMBER}.jtl -e -o report${env.BUILD_NUMBER}"
+                        sh "zip -r report${env.BUILD_NUMBER}.zip report${env.BUILD_NUMBER}"
                         if (config.command) {
                             sh config.command
                         } else {
